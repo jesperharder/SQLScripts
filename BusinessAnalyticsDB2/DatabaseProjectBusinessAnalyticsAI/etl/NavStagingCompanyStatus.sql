6093b953ac7b015f06ab15625def4f2f0e227929
Jesper Harder <jh@scanpan.dk>
2026-04-26 10:21:17 +0200

    NAV historical

---
+++ BusinessAnalyticsDB2/DatabaseProjectBusinessAnalyticsAI/etl/NavStagingCompanyStatus.sql
@@ -0,0 +1,21 @@
+CREATE TABLE [etl].[NavStagingCompanyStatus] (
+    [CompanyID]     TINYINT        NOT NULL,
+    [CountryCode]   VARCHAR (10)   NOT NULL,
+    [StagingStatus] VARCHAR (20)   NOT NULL,
+    [UpdatedAtUtc]  DATETIME2 (0)  NOT NULL,
+    [UpdatedBy]     NVARCHAR (128) NOT NULL,
+    [Comment]       NVARCHAR (400) NULL,
+    CONSTRAINT [PK_NavStagingCompanyStatus] PRIMARY KEY CLUSTERED ([CompanyID] ASC),
+    CONSTRAINT [UQ_NavStagingCompanyStatus_CountryCode] UNIQUE NONCLUSTERED ([CountryCode] ASC),
+    CONSTRAINT [FK_NavStagingCompanyStatus_Company] FOREIGN KEY ([CompanyID]) REFERENCES [etl].[Company] ([CompanyId]),
+    CONSTRAINT [CK_NavStagingCompanyStatus_StagingStatus] CHECK ([StagingStatus]='Historical' OR [StagingStatus]='Operational')
+);
+
+
+GO
+
+CREATE NONCLUSTERED INDEX [IX_NavStagingCompanyStatus_CountryStatus]
+    ON [etl].[NavStagingCompanyStatus]([CountryCode] ASC, [StagingStatus] ASC)
+
+GO
+
